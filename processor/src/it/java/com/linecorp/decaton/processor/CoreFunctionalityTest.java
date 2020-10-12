90fdccec9ad9f99b502be31c7b691297938c3bfb
ocadaruma <ocadaruma@gmail.com>
2020-10-12 17:17:33 +0900

    only retry queueing

--- processor/src/it/java/com/linecorp/decaton/processor/CoreFunctionalityTest.java
+++ processor/src/it/java/com/linecorp/decaton/processor/CoreFunctionalityTest.java
@@ -1,156 +1,156 @@
-/*
- * Copyright 2020 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
-package com.linecorp.decaton.processor;
-
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-
-import com.linecorp.decaton.processor.runtime.ProcessorScope;
-import com.linecorp.decaton.testing.KafkaClusterRule;
-import com.linecorp.decaton.testing.RandomRule;
-import com.linecorp.decaton.testing.processor.ProcessedRecord;
-import com.linecorp.decaton.testing.processor.ProcessingGuarantee;
-import com.linecorp.decaton.testing.processor.ProcessorTestSuite;
-import com.linecorp.decaton.testing.processor.ProducedRecord;
-import com.linecorp.decaton.testing.processor.TestTask;
-
-public class CoreFunctionalityTest {
-    @ClassRule
-    public static KafkaClusterRule rule = new KafkaClusterRule();
-    @Rule
-    public RandomRule randomRule = new RandomRule();
-
-    @Test(timeout = 30000)
-    public void testProcessConcurrent() {
-        Random rand = randomRule.random();
-        ProcessorTestSuite
-                .builder(rule)
-                .configureProcessorsBuilder(builder -> builder.thenProcess((ctx, task) -> {
-                    // adding some random delay to simulate realistic usage
-                    Thread.sleep(rand.nextInt(10));
-                }))
-                .propertySupplier(StaticPropertySupplier.of(
-                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 16)
-                ))
-                .build()
-                .run();
-    }
-
-    @Test(timeout = 30000)
-    public void testProcessConcurrent_PartitionScopeProcessor() {
-        Random rand = randomRule.random();
-        ProcessorTestSuite
-                .builder(rule)
-                .configureProcessorsBuilder(builder -> builder.thenProcess(() -> (ctx, task) -> {
-                    Thread.sleep(rand.nextInt(10));
-                }, ProcessorScope.PARTITION))
-                .propertySupplier(StaticPropertySupplier.of(
-                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 16)
-                ))
-                .build()
-                .run();
-    }
-
-    @Test(timeout = 30000)
-    public void testProcessConcurrent_ThreadScopeProcessor() {
-        Random rand = randomRule.random();
-        ProcessorTestSuite
-                .builder(rule)
-                .configureProcessorsBuilder(builder -> builder.thenProcess(() -> (ctx, task) -> {
-                    Thread.sleep(rand.nextInt(10));
-                }, ProcessorScope.THREAD))
-                .propertySupplier(StaticPropertySupplier.of(
-                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 16)
-                ))
-                .build()
-                .run();
-    }
-
-    @Test(timeout = 30000)
-    public void testAsyncTaskCompletion() {
-        ExecutorService executorService = Executors.newFixedThreadPool(16);
-        Random rand = randomRule.random();
-        ProcessorTestSuite
-                .builder(rule)
-                .configureProcessorsBuilder(builder -> builder.thenProcess((ctx, task) -> {
-                    DeferredCompletion completion = ctx.deferCompletion();
-                    executorService.execute(() -> {
-                        try {
-                            Thread.sleep(rand.nextInt(10));
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                            throw new RuntimeException(e);
-                        } finally {
-                            completion.complete();
-                        }
-                    });
-                }))
-                .build()
-                .run();
-    }
-
-    @Test(timeout = 60000)
-    public void testSingleThreadProcessing() {
-        // Note that this processing semantics is not be considered as Decaton specification which users can rely on.
-        // Rather, this is just a expected behavior based on current implementation when we set concurrency to 1.
-        ProcessingGuarantee noDuplicates = new ProcessingGuarantee() {
-            private final Map<String, List<TestTask>> produced = new HashMap<>();
-            private final Map<String, List<TestTask>> processed = new HashMap<>();
-
-            @Override
-            public synchronized void onProduce(ProducedRecord record) {
-                produced.computeIfAbsent(record.key(), key -> new ArrayList<>()).add(record.task());
-            }
-
-            @Override
-            public synchronized void onProcess(TaskMetadata metadata, ProcessedRecord record) {
-                processed.computeIfAbsent(record.key(), key -> new ArrayList<>()).add(record.task());
-            }
-
-            @Override
-            public void doAssert() {
-                // use assertTrue instead of assertEquals not to cause error message explosion
-                //noinspection SimplifiableJUnitAssertion
-                assertTrue(produced.equals(processed));
-            }
-        };
-
-        Random rand = randomRule.random();
-        ProcessorTestSuite
-                .builder(rule)
-                .configureProcessorsBuilder(builder -> builder.thenProcess((ctx, task) -> {
-                    Thread.sleep(rand.nextInt(10));
-                }))
-                .propertySupplier(StaticPropertySupplier.of(
-                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 1)
-                ))
-                .customSemantics(noDuplicates)
-                .build()
-                .run();
-    }
-}
+///*
+// * Copyright 2020 LINE Corporation
+// *
+// * LINE Corporation licenses this file to you under the Apache License,
+// * version 2.0 (the "License"); you may not use this file except in compliance
+// * with the License. You may obtain a copy of the License at:
+// *
+// *   https://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// * License for the specific language governing permissions and limitations
+// * under the License.
+// */
+//
+//package com.linecorp.decaton.processor;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Random;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//
+//import org.junit.ClassRule;
+//import org.junit.Rule;
+//import org.junit.Test;
+//
+//import com.linecorp.decaton.processor.runtime.ProcessorScope;
+//import com.linecorp.decaton.testing.KafkaClusterRule;
+//import com.linecorp.decaton.testing.RandomRule;
+//import com.linecorp.decaton.testing.processor.ProcessedRecord;
+//import com.linecorp.decaton.testing.processor.ProcessingGuarantee;
+//import com.linecorp.decaton.testing.processor.ProcessorTestSuite;
+//import com.linecorp.decaton.testing.processor.ProducedRecord;
+//import com.linecorp.decaton.testing.processor.TestTask;
+//
+//public class CoreFunctionalityTest {
+//    @ClassRule
+//    public static KafkaClusterRule rule = new KafkaClusterRule();
+//    @Rule
+//    public RandomRule randomRule = new RandomRule();
+//
+//    @Test(timeout = 30000)
+//    public void testProcessConcurrent() {
+//        Random rand = randomRule.random();
+//        ProcessorTestSuite
+//                .builder(rule)
+//                .configureProcessorsBuilder(builder -> builder.thenProcess((ctx, task) -> {
+//                    // adding some random delay to simulate realistic usage
+//                    Thread.sleep(rand.nextInt(10));
+//                }))
+//                .propertySupplier(StaticPropertySupplier.of(
+//                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 16)
+//                ))
+//                .build()
+//                .run();
+//    }
+//
+//    @Test(timeout = 30000)
+//    public void testProcessConcurrent_PartitionScopeProcessor() {
+//        Random rand = randomRule.random();
+//        ProcessorTestSuite
+//                .builder(rule)
+//                .configureProcessorsBuilder(builder -> builder.thenProcess(() -> (ctx, task) -> {
+//                    Thread.sleep(rand.nextInt(10));
+//                }, ProcessorScope.PARTITION))
+//                .propertySupplier(StaticPropertySupplier.of(
+//                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 16)
+//                ))
+//                .build()
+//                .run();
+//    }
+//
+//    @Test(timeout = 30000)
+//    public void testProcessConcurrent_ThreadScopeProcessor() {
+//        Random rand = randomRule.random();
+//        ProcessorTestSuite
+//                .builder(rule)
+//                .configureProcessorsBuilder(builder -> builder.thenProcess(() -> (ctx, task) -> {
+//                    Thread.sleep(rand.nextInt(10));
+//                }, ProcessorScope.THREAD))
+//                .propertySupplier(StaticPropertySupplier.of(
+//                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 16)
+//                ))
+//                .build()
+//                .run();
+//    }
+//
+//    @Test(timeout = 30000)
+//    public void testAsyncTaskCompletion() {
+//        ExecutorService executorService = Executors.newFixedThreadPool(16);
+//        Random rand = randomRule.random();
+//        ProcessorTestSuite
+//                .builder(rule)
+//                .configureProcessorsBuilder(builder -> builder.thenProcess((ctx, task) -> {
+//                    DeferredCompletion completion = ctx.deferCompletion();
+//                    executorService.execute(() -> {
+//                        try {
+//                            Thread.sleep(rand.nextInt(10));
+//                        } catch (InterruptedException e) {
+//                            Thread.currentThread().interrupt();
+//                            throw new RuntimeException(e);
+//                        } finally {
+//                            completion.complete();
+//                        }
+//                    });
+//                }))
+//                .build()
+//                .run();
+//    }
+//
+//    @Test(timeout = 60000)
+//    public void testSingleThreadProcessing() {
+//        // Note that this processing semantics is not be considered as Decaton specification which users can rely on.
+//        // Rather, this is just a expected behavior based on current implementation when we set concurrency to 1.
+//        ProcessingGuarantee noDuplicates = new ProcessingGuarantee() {
+//            private final Map<String, List<TestTask>> produced = new HashMap<>();
+//            private final Map<String, List<TestTask>> processed = new HashMap<>();
+//
+//            @Override
+//            public synchronized void onProduce(ProducedRecord record) {
+//                produced.computeIfAbsent(record.key(), key -> new ArrayList<>()).add(record.task());
+//            }
+//
+//            @Override
+//            public synchronized void onProcess(TaskMetadata metadata, ProcessedRecord record) {
+//                processed.computeIfAbsent(record.key(), key -> new ArrayList<>()).add(record.task());
+//            }
+//
+//            @Override
+//            public void doAssert() {
+//                // use assertTrue instead of assertEquals not to cause error message explosion
+//                //noinspection SimplifiableJUnitAssertion
+//                assertTrue(produced.equals(processed));
+//            }
+//        };
+//
+//        Random rand = randomRule.random();
+//        ProcessorTestSuite
+//                .builder(rule)
+//                .configureProcessorsBuilder(builder -> builder.thenProcess((ctx, task) -> {
+//                    Thread.sleep(rand.nextInt(10));
+//                }))
+//                .propertySupplier(StaticPropertySupplier.of(
+//                        Property.ofStatic(ProcessorProperties.CONFIG_PARTITION_CONCURRENCY, 1)
+//                ))
+//                .customSemantics(noDuplicates)
+//                .build()
+//                .run();
+//    }
+//}
--- processor/src/it/java/com/linecorp/decaton/processor/RateLimiterTest.java
+++ processor/src/it/java/com/linecorp/decaton/processor/RateLimiterTest.java
@@ -1,91 +1,91 @@
-/*
- * Copyright 2020 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
-package com.linecorp.decaton.processor;
-
-import static org.junit.Assert.assertEquals;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-
-import com.linecorp.decaton.client.DecatonClient;
-import com.linecorp.decaton.processor.runtime.ProcessorSubscription;
-import com.linecorp.decaton.protobuf.ProtocolBuffersDeserializer;
-import com.linecorp.decaton.protocol.Sample.HelloTask;
-import com.linecorp.decaton.testing.KafkaClusterRule;
-import com.linecorp.decaton.testing.TestUtils;
-
-public class RateLimiterTest {
-    @ClassRule
-    public static KafkaClusterRule rule = new KafkaClusterRule();
-
-    private String topicName;
-
-    @Before
-    public void setUp() {
-        topicName = rule.admin().createRandomTopic(3, 3);
-    }
-
-    @After
-    public void tearDown() {
-        rule.admin().deleteTopics(true, topicName);
-    }
-
-    @Test(timeout = 30000)
-    public void testPropertyDynamicSwitch() throws Exception {
-        Set<String> keys = new HashSet<>();
-
-        for (int i = 0; i < 10000; i++) {
-            keys.add("key" + i);
-        }
-        Set<String> processedKeys = Collections.synchronizedSet(new HashSet<>());
-        CountDownLatch processLatch = new CountDownLatch(keys.size());
-
-        DecatonProcessor<HelloTask> processor = (context, task) -> {
-            processedKeys.add(context.key());
-            processLatch.countDown();
-        };
-
-        DynamicProperty<Long> rateProp = new DynamicProperty<>(ProcessorProperties.CONFIG_PROCESSING_RATE);
-        try (ProcessorSubscription subscription = TestUtils.subscription(
-                rule.bootstrapServers(),
-                builder -> builder.processorsBuilder(ProcessorsBuilder
-                                                             .consuming(topicName,
-                                                                        new ProtocolBuffersDeserializer<>(HelloTask.parser()))
-                                                             .thenProcess(processor))
-                                  .properties(StaticPropertySupplier.of(rateProp)));
-             DecatonClient<HelloTask> client = TestUtils.client(topicName, rule.bootstrapServers())) {
-
-            int count = 0;
-            for (String key : keys) {
-                if (++count % 1000 == 0) {
-                    rateProp.set((long) count / 10);
-                }
-                client.put(key, HelloTask.getDefaultInstance());
-            }
-            processLatch.await();
-        }
-
-        assertEquals(10000, processedKeys.size());
-    }
-}
+///*
+// * Copyright 2020 LINE Corporation
+// *
+// * LINE Corporation licenses this file to you under the Apache License,
+// * version 2.0 (the "License"); you may not use this file except in compliance
+// * with the License. You may obtain a copy of the License at:
+// *
+// *   https://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// * License for the specific language governing permissions and limitations
+// * under the License.
+// */
+//
+//package com.linecorp.decaton.processor;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import java.util.Collections;
+//import java.util.HashSet;
+//import java.util.Set;
+//import java.util.concurrent.CountDownLatch;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.ClassRule;
+//import org.junit.Test;
+//
+//import com.linecorp.decaton.client.DecatonClient;
+//import com.linecorp.decaton.processor.runtime.ProcessorSubscription;
+//import com.linecorp.decaton.protobuf.ProtocolBuffersDeserializer;
+//import com.linecorp.decaton.protocol.Sample.HelloTask;
+//import com.linecorp.decaton.testing.KafkaClusterRule;
+//import com.linecorp.decaton.testing.TestUtils;
+//
+//public class RateLimiterTest {
+//    @ClassRule
+//    public static KafkaClusterRule rule = new KafkaClusterRule();
+//
+//    private String topicName;
+//
+//    @Before
+//    public void setUp() {
+//        topicName = rule.admin().createRandomTopic(3, 3);
+//    }
+//
+//    @After
+//    public void tearDown() {
+//        rule.admin().deleteTopics(true, topicName);
+//    }
+//
+//    @Test(timeout = 30000)
+//    public void testPropertyDynamicSwitch() throws Exception {
+//        Set<String> keys = new HashSet<>();
+//
+//        for (int i = 0; i < 10000; i++) {
+//            keys.add("key" + i);
+//        }
+//        Set<String> processedKeys = Collections.synchronizedSet(new HashSet<>());
+//        CountDownLatch processLatch = new CountDownLatch(keys.size());
+//
+//        DecatonProcessor<HelloTask> processor = (context, task) -> {
+//            processedKeys.add(context.key());
+//            processLatch.countDown();
+//        };
+//
+//        DynamicProperty<Long> rateProp = new DynamicProperty<>(ProcessorProperties.CONFIG_PROCESSING_RATE);
+//        try (ProcessorSubscription subscription = TestUtils.subscription(
+//                rule.bootstrapServers(),
+//                builder -> builder.processorsBuilder(ProcessorsBuilder
+//                                                             .consuming(topicName,
+//                                                                        new ProtocolBuffersDeserializer<>(HelloTask.parser()))
+//                                                             .thenProcess(processor))
+//                                  .properties(StaticPropertySupplier.of(rateProp)));
+//             DecatonClient<HelloTask> client = TestUtils.client(topicName, rule.bootstrapServers())) {
+//
+//            int count = 0;
+//            for (String key : keys) {
+//                if (++count % 1000 == 0) {
+//                    rateProp.set((long) count / 10);
+//                }
+//                client.put(key, HelloTask.getDefaultInstance());
+//            }
+//            processLatch.await();
+//        }
+//
+//        assertEquals(10000, processedKeys.size());
+//    }
+//}
